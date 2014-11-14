7e233fb236256130e507dab575cc4f9462a64675
vgj1 <juergen.vogel@bfh.ch>
2014-11-14 11:08:44 +0100

    vaadin setup

---
+++ src/main/webapp/VAADIN/widgetsets/ch.bfh.btx8081.VaadinDemo.AppWidgetSet/deferredjs/9E6FF08E5EA2349637CF3BFBE48D1916/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.ch_bfh_btx8081_VaadinDemo_AppWidgetSet.runAsyncCallback2('T8(1547,1,jkd);_.vc=function S_b(){gRb((!_Qb&&(_Qb=new lRb),_Qb),this.b.e)};Ned(Vh)(2);\n//# sourceURL=ch.bfh.btx8081.VaadinDemo.AppWidgetSet-2.js\n')
